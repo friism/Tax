a0e1f100bb2a13862e20fe7ef1576c875892214f
Michael Friis <friism@gmail.com>
2012-12-25 15:09:38 -0800

    extend unit tests

--- Tax.Test/DataExtractorTests.cs
+++ Tax.Test/DataExtractorTests.cs
@@ -9,9 +9,11 @@ namespace Tax.Test
 	public class DataExtractorTests
 	{
 		[Theory]
-		[InlineData(29604274, "FORLAGET SOHN ApS")]
-		[InlineData(15731249, "SAXO BANK A/S")]
-		public void Test(int cvrNumber, string name)
+		[InlineData(29604274, "FORLAGET SOHN ApS", 0L, -52598L, null, null, null, null)]
+		[InlineData(15731249, "SAXO BANK A/S", 25426135L, 257969357L, null, null, null, null)]
+		[InlineData(87197719, "SHELL OLIE- OG GASUDVINDING DANMARK B.V. (HOLLAND), DANSK  FILIAL", 3825964525L, 15303858194L, null, 5213476840L, 10025917000L, 0L)]
+		[InlineData(29520852, "NORECO DENMARK A/S", 185342975L, 741371927L, 12179356L, 0L, -72864162L, 119751074L)]
+		public void Test(int cvrNumber, string name, long? tax, long? profit, long? losses, long? fossilTax, long? fossilProfit, long? fossilLosses)
 		{
 			var contentHelper = new ContentHelper(Encoding.GetEncoding("ISO-8859-1"));
 			var document = contentHelper.GetContent(cvrNumber);
@@ -21,6 +23,14 @@ namespace Tax.Test
 			extractor.Extract(company, document);
 
 			Assert.Equal(name, company.Name);
+
+			Assert.Equal((decimal?)tax, company.TaxPaid);
+			Assert.Equal((decimal?)profit, company.Revenue);
+			Assert.Equal((decimal?)losses, company.Losses);
+
+			Assert.Equal((decimal?)fossilTax, company.FossilTaxPaid);
+			Assert.Equal((decimal?)fossilProfit, company.FossilProfit);
+			Assert.Equal((decimal?)fossilLosses, company.FossilLosses);
 		}
 	}
 }
